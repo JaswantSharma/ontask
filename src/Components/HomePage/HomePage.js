a225e9b1d08e2d8963b98317946682100d41dd6c
githubchalatahu <77007044+githubchalatahu@users.noreply.github.com>
2021-12-28 19:54:47 +0530

    Update HomePage.js

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -2,8 +2,7 @@ import React, { Component } from "react";
 import Navbar from "./Sections/Navbar"
 import Banner from "./Sections/Banner";
 import KnowAbout from "./Sections/KnowAbout";
-import JobSeeker from "./Sections/JobSeeker";
-import Employer from "./Sections/Employer";
+
 
 class HomePage extends Component {
   render() {
